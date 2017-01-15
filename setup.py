8c76069f310910315549d594ee187b711b891ebf
FRANCESCO PAOLO DELLAQUILA <paolodellaquila1999@gmail.com>
2017-01-15 21:28:42 +0100

    prepare package for Pypi

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+    name='Prometheus',
+    version='0.1',
+    packages=['Prometheus'],
+    url='https://github.com/Francescopaolo44/Prometheus',
+    license='Apache',
+    author='paolo',
+    author_email='paolodellaquila1999@gmail.com',
+    description='command line tool to track #100daysofcode progress',
+
+     classifiers=[
+        'Development Status :: 5 - Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: Apache License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='sample tracker progress',
+
+)
